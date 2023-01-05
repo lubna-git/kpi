bef28da26f31d051299b66d9cb7f088fcd7796cd
Lubna82-git <lubnahameed82@gmai.com>
2023-01-05 15:18:28 +0300

    first commit

--- kpi/kpi/doctype/kpi_evaluation/kpi_evaluation.js
+++ kpi/kpi/doctype/kpi_evaluation/kpi_evaluation.js
@@ -2,8 +2,9 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('KPI Evaluation', {
-	// filter the task depande on Employee name
+	// on change the value of employee field name
 		employee: function(frm) {
+			// fillter the task field name value depened on user field name
 			cur_frm.set_query("task", function(){
 				return {
 					"filters": {
@@ -11,9 +12,9 @@ frappe.ui.form.on('KPI Evaluation', {
 					} 
 				}
 			})},
-		// get tabel of kpi template auto
-		
-		kpi_template: async function(frm) {	
+	// on change the value of KPI Template field name
+	kpi_template: async function(frm) {	
+		// get kpi list with choose kpi template
 			let doc = await frappe.db.get_doc("KPI Template",frm.doc.kpi_template)
 			doc.kpi.forEach(function(x){cur_frm.add_child("kpi",{kpi:x.kpi,description:x.description,score:x.score})});
 			frm.refresh()
