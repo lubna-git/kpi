0d6bdb6e9c1f9ff842358de95f51d644a00d72ac
osama1998H <osamamuhammed555@gmail.com>
2023-01-15 10:39:23 +0300

    ...

--- kpi/kpi/doctype/kpi_evaluation/kpi_evaluation.py
+++ kpi/kpi/doctype/kpi_evaluation/kpi_evaluation.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2023, Lubna Hameed and contributors
 # For license information, please see license.txt
 
-import frappe
+from frappe import _, msgprint, throw
 from frappe.model.document import Document
 
 class KPIEvaluation(Document):
 	def before_save(self):
 		for i in self.kpi:
 			if i.evaluation > i.score:
-				frappe.throw("Evaluation Can't Be Larger Than Score")
+				throw(_("Evaluation Can't Be Larger Than Score"))
